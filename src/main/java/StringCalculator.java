764575dcd089865941e0e2c17212fb35db28c059
Sergey Smirnov <ssmirnov@natera.com>
2017-12-29 15:55:29 +0300

    Refactoring.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,10 @@
 import java.util.Arrays;
-import java.util.List;
 
 public class StringCalculator {
     public int add(String input) {
         if (input.equals(""))
             return 0;
 
-        List<String> strings = Arrays.asList(input.split(","));
-
-        int sum = 0;
-        for (String s : strings) {
-            sum += Integer.parseInt(s);
-        }
-
-        return sum;
+        return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
